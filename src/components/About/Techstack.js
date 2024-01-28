968f3278c63fb5b018824aee70d14d2db8e29c51
sejal99 <sejal8974@gmail.com>
2024-01-28 19:40:09 +0530

    new commit

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -8,34 +8,69 @@ import {
   DiMongodb,
   DiExpress,
   DiGit,
+  DiCss3,
+  DiBootstrap,
   
 } from "react-icons/di";
+import { SiDocker, SiTypescript } from "react-icons/si";
+import { TbBrandNextjs } from "react-icons/tb";
 
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <CgCPlusPlus />
+     
+
+
+     <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
       </Col>
+
+     <Col xs={4} md={2} className="tech-icons">
+        <DiNodejs />
+      </Col>
+
       <Col xs={4} md={2} className="tech-icons">
-        <DiJavascript1 />
+      <TbBrandNextjs />
+
       </Col>
       
       <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
+        <DiJavascript1 />
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
-        <DiReact />
+      <SiTypescript />
       </Col>
+      
+    
       
       <Col xs={4} md={2} className="tech-icons">
         <DiMongodb />
       </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        
+      <SiDocker />
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <DiCss3 />
+      </Col>
     
+      <Col xs={4} md={2} className="tech-icons">
+        <CgCPlusPlus />
+      </Col>
+
       <Col xs={4} md={2} className="tech-icons">
         <DiGit />
       </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+      
+      <DiBootstrap />
+      </Col>
+
     
      
       
